b08629ca2aae15d6a493b37f6a50d390c86a2a62
vabanga <gohavorsin@gmail.com>
2017-08-23 04:35:10 +1000

    end

--- weather.php
+++ weather.php
@@ -3,7 +3,7 @@
 $pogoda = "http://samples.openweathermap.org/data/2.5/weather?q=Khabarovsk,ru&appid=6b7c151fcd9ea5d60237fef747ad775c";
 $string = file_get_contents($pogoda);
 
-$results = json_decode($string, $assoc = true, $depth = 10000);
+$results = json_decode($string, true);
 
 foreach($results["weather"][0] as $k=>$v)
 {
